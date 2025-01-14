0ea12778b34d5ff6de4111a5cca4c0c28061d13f
Vengiro <ibmoussab10@gmail.com>
2025-01-14 14:15:20 +0100

    Poetry and cuda not a good mix so switch on pip to manage dependencies. CUDA at least 5 times faster

--- src/main.py
+++ src/main.py
@@ -4,7 +4,13 @@ import torchvision
 import torch
 import argparse
 def main(epoch, batch_size, lr):
-    print(torch.cuda.is_available())
+    print(torch.__version__)  # Check PyTorch version
+    print(torch.cuda.is_available())  # Should return True if CUDA is available
+    print(torch.cuda.device_count())  # Number of GPUs detected
+    print(torch.cuda.current_device())  # The ID of the current GPU (if available)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cpu")
+    print(f"Using device: {device}")
     train_data = torchvision.datasets.MNIST(
         root='./data',
         train=True,
@@ -25,20 +31,22 @@ def main(epoch, batch_size, lr):
         shuffle=True
     )
 
-    generator = Generator.Generator_MNIST()
-    discriminator = Discriminator.Discriminator_MNIST()
+    generator = Generator.Generator_MNIST().to(device)
+    discriminator = Discriminator.Discriminator_MNIST().to(device)
     lossFunc = torch.nn.BCELoss()
     gen_optimizer = torch.optim.Adam(generator.parameters(), lr=lr)
     discr_optimizer = torch.optim.Adam(discriminator.parameters(), lr=lr)
 
     for i  in range(epoch):
         for j, (img, _) in enumerate(train_loader):
+            img = img.to(device)
+            noise = torch.randn(batch_size, 100).to(device)
             # Train Discriminator
             discr_optimizer.zero_grad()
             real_img = img
-            real_label = torch.ones(batch_size, 1)
-            fake_label = torch.zeros(batch_size, 1)
-            fake_img = generator.forward(torch.randn(batch_size, 100))
+            real_label = torch.ones(batch_size, 1, device=device)
+            fake_label = torch.zeros(batch_size, 1, device=device)
+            fake_img = generator.forward(noise)
             real_out = discriminator.forward(real_img)
             fake_out = discriminator.forward(fake_img)
             real_loss = lossFunc(real_out, real_label)
@@ -49,7 +57,8 @@ def main(epoch, batch_size, lr):
 
             # Train Generator
             gen_optimizer.zero_grad()
-            fake_img = generator.forward(torch.randn(batch_size, 100))
+            noise = torch.randn(batch_size, 100).to(device)
+            fake_img = generator.forward(noise)
             fake_out = discriminator.forward(fake_img)
             gen_loss = lossFunc(fake_out, real_label)
             gen_loss.backward()
@@ -68,7 +77,7 @@ def main(epoch, batch_size, lr):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epoch", type=int, default=5)
+    parser.add_argument("--epoch", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=100)
     parser.add_argument("--lr", type=float, default=0.0002)
     args = parser.parse_args()
